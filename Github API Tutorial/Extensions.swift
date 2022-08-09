ecca0cc386732b5a75dc59cf4a86a73d86c1f742
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-08-09 15:58:13 +0500

    Created Extension for UIView and UIImageView.

---
+++ Github API Tutorial/Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  Extensions.swift
+//  iOS Test Assignment
+//
+//  Created by Hamza Rafique Azad on 10/5/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
